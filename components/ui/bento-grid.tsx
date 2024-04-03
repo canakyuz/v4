368a546ae2ceb4a5771e8e1b91fe10e49b70742a
canakyuz <bekircanakyuz@gmail.com>
2024-04-03 14:54:29 +0300

    fix grid

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -14,7 +14,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
+        "grid md:auto-rows-auto grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
         className
       )}
     >
@@ -44,7 +44,7 @@ export const BentoGridItem = ({ props, className }: BentoGridItemProps) => {
   return (
     <Link href={linkHref}>
       <div className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 border-card bg-background border-2 justify-between flex flex-col space-y-4 animate-in font-body",
+        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 border-card bg-card border-2 justify-between flex flex-col space-y-4 animate-in font-body",
         className
       )}>
         <Image
